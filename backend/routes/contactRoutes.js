21abf32e8b41acd6a99c3b4f98a0d1496730e922
Crayflee <julian.thuering@t-online.de>
2024-09-01 21:16:01 +0100

    api routes added

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -7,5 +7,9 @@ const contactController = require('../controllers/contactController');
 
 router.post('/contacts', contactController.createContact);
 router.get('/contacts', contactController.getAllContacts);
+router.delete('/contacts/:id', contactController.deleteContact);
+router.put('/contacts/:id', contactController.updateContact);
+
+
 
 module.exports = router;
